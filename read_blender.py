4ee4310a9a6e3cf0454d1cae1caf2e34c90e50a6
Kevin THIOMBIANO <thiombianokevin@gmail.com>
2019-02-21 00:16:24 +0100

    Working on blender download

--- read_blender.py
+++ read_blender.py
@@ -1,11 +1,12 @@
 import urllib.request
 import re
 
-download_url = 'https://www.blender.org/download/'
+download_url = 'https://builder.blender.org/download/'
 
 with urllib.request.urlopen(download_url) as response:
    html = response.read()
 
-encoding = re.findall(r'<meta.*?charset=["\']*(.+?)["\'>]', html, flags=re.I)[0]
+encoding = re.findall(r'<meta.*?charset=["\']*(.+?)["\'>]', html.decode(), flags=re.I)[0]
 
 print(encoding)
+
