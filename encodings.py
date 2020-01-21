db79cabb00abfdf27bd5f381babc39575e4602cf
Kevin THIOMBIANO <thiombianokevin@gmail.com>
2020-01-21 11:24:48 +0100

    Removing encodoing script

--- encodings.py
+++
@@ -1,6 +0,0 @@
-import encodings
-
-# print(''.join('- ' + e + '\n' for e in sorted(set(encodings.aliases.aliases.values()))))
-# print(len(encodings.aliases.aliases.keys()))
-
-print(type("test".encode()))
